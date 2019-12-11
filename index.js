c05f3d598f865e1a11672eac0b8923f52b206be2
Arvida Jimenez <arvida.jimenez@gmail.com>
2019-12-11 20:00:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const append
+const append = ["Milo", "Otis", "Garfield"];
+const prepend = ["Milo", "Otis", "Garfield"];
+const removeLast = ["Milo", "Otis", "Garfield"];
+const removeFirst = ["Milo", "Otis", "Garfield"];
 
 
 function append(petsArray) {
